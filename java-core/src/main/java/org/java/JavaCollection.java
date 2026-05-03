755a0c5c604cf9be175b5b523007177674d2faec
praveenamsa1310 <praveenamsa1310@gmail.com>
2026-05-03 13:30:38 +0530

    data time

---
+++ java-core/src/main/java/org/java/JavaCollection.java
@@ -0,0 +1,4 @@
+package org.java;
+
+public class JavaCollection {
+}
--- java-core/src/main/java/org/java/javaclass/JavaDateAndTime.java
+++ java-core/src/main/java/org/java/javaclass/JavaDateAndTime.java
@@ -1,4 +1,26 @@
 package org.java.javaclass;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class JavaDateAndTime {
+
+    public static void main(String[] args) {
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        System.out.println("Current Year : " + localDateTime);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        System.out.println("Formated Date : " + formatter.format(localDateTime));
+
+        String inputDate = "2026-01-01";
+
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate localDate = LocalDate.parse(inputDate, inputFormatter);
+
+        System.out.println("Input Date : " + localDate);
+    }
 }
